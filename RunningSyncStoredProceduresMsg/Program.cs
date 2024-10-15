1ca6c037b21f06505bd81e2cdf4b5af6b1f8b07b
David Ayrolla <davidayrolla@gmail.com>
2024-10-15 15:30:32 -0300

    Add project files.

---
+++ RunningSyncStoredProceduresMsg/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Put your Connection String here";
+        string storedProcedureName = "RunningProcedure";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                // Registrar o evento para capturar as mensagens
+                connection.InfoMessage += new SqlInfoMessageEventHandler(OnInfoMessage);
+
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(storedProcedureName, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    Console.WriteLine("Calling the stored procedure...");
+
+                    // Executar a stored procedure (essa chamada é síncrona).
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("Stored procedure finished.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+        }
+    }
+
+    // Manipulador de eventos para capturar mensagens enviadas com RAISERROR ou PRINT
+    static void OnInfoMessage(object sender, SqlInfoMessageEventArgs e)
+    {
+        // Exibir as mensagens de progresso da stored procedure
+        Console.WriteLine("Message from SQL Server: " + e.Message);
+    }
+}
---
+++ RunningSyncStoredProceduresMsg/RunningProcedure.sql
@@ -0,0 +1,30 @@
+﻿CREATE OR ALTER PROCEDURE RunningProcedure
+AS
+BEGIN
+    -- Enviar mensagem parcial (progresso)
+    RAISERROR('Process beginning...', 0, 1) WITH NOWAIT;
+
+    -- Simula uma operação demorada
+    WAITFOR DELAY '00:00:05';
+
+    -- Enviar outra mensagem parcial
+    RAISERROR('25%% of process...', 0, 1) WITH NOWAIT;
+
+    -- Simula mais uma operação demorada
+    WAITFOR DELAY '00:00:05';
+
+    -- Enviar outra mensagem parcial
+    RAISERROR('50%% of process...', 0, 1) WITH NOWAIT;
+
+    -- Simula mais uma operação demorada
+    WAITFOR DELAY '00:00:05';
+   
+    -- Enviar outra mensagem parcial
+    RAISERROR('75%%... of process', 0, 1) WITH NOWAIT;
+
+    -- Simula mais uma operação demorada
+    WAITFOR DELAY '00:00:05';   
+
+    -- Enviar mensagem final
+    RAISERROR('Process finished!', 0, 1) WITH NOWAIT;
+END;
